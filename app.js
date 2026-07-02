24bff51af70531b5c6dda374d886008f20e58bcd
May Thinzar Htet <mthinzar2005@gmail.com>
2026-07-02 18:11:21 +0800

    Complete Student List App

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+// Create MySQL connection 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'RP738964$',
+  database: 'c237_studentlistapp'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+// Set up view engine 
+app.set('view engine', 'ejs');
+//  enable static files 
+app.use(express.static('public'));
+// enable form processing for POST requests
+app.use(express.urlencoded({ extended: false }));
+
+
+// Define routes
+app.get('/', (req, res) => {
+  const sql = 'SELECT * FROM student';
+  // Fetch data from MySQL
+  connection.query(sql, (error, results) => {
+    if (error) {
+      console.error('Database query error:', error.message);
+      return res.send('Error Retrieving students');
+    }
+    // Render HTML page with data
+    res.render('index', { students: results });
+  });
+});
+
+app.get('/student/:id', (req, res) => {
+  const studentid = req.params.id;
+
+  const sql = 'SELECT * FROM student WHERE studentid = ?';
+
+  connection.query(sql, [studentid], (error, results) => {
+    if (error) {
+      console.error('Database query error:', error.message);
+      return res.send('Error Retrieving student by ID');
+    }
+
+    if (results.length > 0) {
+      res.render('student', { student: results[0] });
+    } else {
+      res.send('Student not found');
+    }
+  });
+});
+
+app.get('/addStudent', (req, res) => {
+    res.render('addStudent');
+});
+
+app.post('/addStudent', (req, res) => {
+    // Extract student data from the request body
+    const { name, dob, contact, image } = req.body;
+
+    const sql = `
+        INSERT INTO student (name, dob, contact, image)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    // Insert the new student into the database
+    connection.query(sql, [name, dob, contact, image], (error, results) => {
+        if (error) {
+            console.error("Error adding student:", error);
+            res.send("Error adding student");
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`)); 
---
+++ c237_studentlistapp_student.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `c237_studentlistapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `c237_studentlistapp`;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: c237_studentlistapp
+-- ------------------------------------------------------
+-- Server version	8.4.6
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `studentid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `dob` date NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `image` varchar(1000) NOT NULL,
+  PRIMARY KEY (`studentid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'John Edward','2007-05-08','98765432','https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww'),(2,'Mary Thomson','2006-02-06','98765678','https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww'),(3,'Benny Teo','2006-08-18','98789879','https://plus.unsplash.com/premium_photo-1689977807477-a579eda91fa2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-02 12:04:27
